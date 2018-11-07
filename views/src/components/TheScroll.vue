1c14b032676d77c3088a58f8546ba6f84cb6e956
liu.liang <492140398@qq.com>
2018-11-07 22:31:12 +0800

    ll

--- views/src/components/TheScroll.vue
+++ views/src/components/TheScroll.vue
@@ -69,85 +69,88 @@ export default {
     }
 
   },
-  data() {
+  data () {
     return {
       // scroll: null
     }
   },
-  mounted() {
-    setTimeout( () => {
+  mounted () {
+    setTimeout(() => {
       this._initScroll()
-    }, 20 )
+    }, 20)
   },
   watch: {
     // 监听数据的变化，延时refreshDelay时间后调用refresh方法重新计算，保证滚动效果正常
-    data() {
-      setTimeout( () => {
+    data () {
+      setTimeout(() => {
         this.refresh()
-      }, this.refreshDelay )
+      }, this.refreshDelay)
     }
   },
   methods: {
-    _initScroll() {
-      if ( !this.$refs.theScroll ) {
+    _initScroll () {
+      if (!this.$refs.theScroll) {
         return
+      }
+      const pullDownRefresh = {
+        threshold: 50,
+        stop: 0
       }
       let options = {
         propbeType: this.propbeType,
+        pullDownRefresh,
         click: this.click,
         scrollX: this.scrollX
       }
-      this.scroll = new BScroll( this.$refs.theScroll, options )
+      this.scroll = new BScroll(this.$refs.theScroll, options)
       // 是否派发滚动事件
-      if ( this.listenScroll ) {
-        this.scroll.on( 'scroll', ( pos ) => {
-          this.$emit( 'scroll', pos )
-        } )
+      if (this.listenScroll) {
+        this.scroll.on('scroll', (pos) => {
+          this.$emit('scroll', pos)
+        })
       }
       // 上拉加载事件
-      if ( this.pullUp ) {
-        this.scroll.on( 'pullingUp', () => {
-          if ( this.scroll.y <= ( this.scroll.maxScrollY + this.bottomDistance ) ) {
-            this.$emit( 'pullingUp' )
+      if (this.pullUp) {
+        this.scroll.on('pullingUp', () => {
+          if (this.scroll.y <= (this.scroll.maxScrollY + this.bottomDistance)) {
+            this.$emit('pullingUp')
           }
-        } )
+        })
       }
 
       // 下拉刷新事件
-      if ( this.pullDown ) {
-        this.scroll.on( 'pullingDown', ( pos ) => {
-          if ( pos.y > this.topDistance ) {
-            this.$emit( 'pullingDown' )
-          }
-        } )
+      if (this.pullDown) {
+        this.scroll.on('pullingDown', () => {
+          this.$emit('pullingDown')
+        })
       }
 
       // 列表滚动开始前的事件
-      if ( this.beforeScroll ) {
-        this.scroll.on( 'beforeScrollStart', () => {
-          this.$emit( 'beforeScroll' )
-        } )
+      if (this.beforeScroll) {
+        this.scroll.on('beforeScrollStart', () => {
+          this.$emit('beforeScroll')
+        })
       }
     },
-    disable() {
+    disable () {
       // 代理better-scroll的disable方法
       this.scroll && this.scroll.disable()
     },
-    enable() {
+    enable () {
       // 代理better-scroll的enable方法
       this.scroll && this.scroll.enable()
     },
-    refresh() {
+    refresh () {
       // 代理better-scroll的refresh方法
       this.scroll && this.scroll.refresh()
     },
-    scrollTo() {
+    scrollTo () {
       // 代理better-scroll的scrollTo方法
-      this.scroll && this.scroll.scrollTo.apply( this.scroll, arguments )
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
     },
-    scrollToElement() {
+    scrollToElement () {
       // 代理better-scroll的scrollToElement方法
-      this.scroll && this.scroll.scrollToElement.apply( this.scroll, arguments )
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
     }
   }
 }
