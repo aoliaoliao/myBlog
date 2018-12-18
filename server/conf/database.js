9796fa14422634a44af3e8f1cfc3aa0d9426cde6
liu.liang <492140398@qq.com>
2018-12-18 18:53:09 +0800

    ll

--- server/conf/database.js
+++ server/conf/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
-        host: 'localhost',
+        host: '47.101.150.40',
         username: 'root',
         password: '123456',
         database: 'nodejs',
