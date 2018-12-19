ace3737bcd74100b1e83b862c3ef34417a714f05
liu.liang <492140398@qq.com>
2018-12-19 20:35:29 +0800

    ll

--- server/controller/momentController/controller/addMoment.js
+++ server/controller/momentController/controller/addMoment.js
@@ -4,113 +4,112 @@ const fs = require('fs')
 const util = require('util')
 const { formatResponse } = require('../../../utils')
 const { momentConst, staticPublicPath } = require('../../../conf')[
-  'gloableConst'
+    'gloableConst'
 ]
 const userId = '0120f580-f92a-11e8-8db7-791c9005fcff'
 
 // 请求内容的验证
 function validateMoment(moment) {
-  let isValidate = true
-  let message = ''
-  const text = moment.text || ''
-  const imgs = moment.imgs || []
-  const video = moment.video
+    let isValidate = true
+    let message = ''
+    const text = moment.text || ''
+    const imgs = moment.imgs || []
+    const video = moment.video
 
-  if (text.length === 0 && imgs.length === 0) {
-    isValidate = false
-    message = '动态内容不允许为空'
-  } else if (text.length > momentConst.textMaxLength) {
-    isValidate = false
-    message = '字数需在200字以内'
-  } else if (imgs.length > momentConst.imgMaxCount) {
-    isValidate = false
-    message = '最多上传九张图片'
-  } else if (video) {
-  }
+    if (text.length === 0 && imgs.length === 0) {
+        isValidate = false
+        message = '动态内容不允许为空'
+    } else if (text.length > momentConst.textMaxLength) {
+        isValidate = false
+        message = '字数需在200字以内'
+    } else if (imgs.length > momentConst.imgMaxCount) {
+        isValidate = false
+        message = '最多上传九张图片'
+    } else if (video) {}
 
-  imgs.some(img => {
-    let right_mime = !momentConst.imgMIMeTypes.includes(img.type)
-    if (right_mime) {
-      isValidate = false
-      message = `只接受${momentConst.imgMIMeTypes.join(' ')}类型的图片`
-    }
-    return right_mime
-  })
+    imgs.some(img => {
+        let right_mime = !momentConst.imgMIMeTypes.includes(img.type)
+        if (right_mime) {
+            isValidate = false
+            message = `只接受${momentConst.imgMIMeTypes.join(' ')}类型的图片`
+        }
+        return right_mime
+    })
 
-  return { isValidate, message }
+    return { isValidate, message }
 }
 
 // 图片存储的目标位置
 async function createTargetDir() {
-  const date = new Date()
-  targetDir = `${staticPublicPath}/${date.getFullYear()}_${date.getMonth() +
+    const date = new Date()
+    targetDir = `${staticPublicPath}/${date.getFullYear()}_${date.getMonth() +
     1}/${date.getDate()}`
 
-  if (!fs.existsSync(targetDir)) {
-    const mkdir = utils.promisify(fs.mkdir)
-    let dirErr = await mkdir(targetDir, {
-      recursive: true
-    })
-    if (dirErr) {
-      targetDir = `${staticPublicPath}`
-      console.log(` ${date} : moment: createTargetDir: 新建文件夹出错 : ${err}`)
+    if (!fs.existsSync(targetDir)) {
+        const mkdir = util.promisify(fs.mkdir)
+        let dirErr = await mkdir(targetDir, {
+            recursive: true
+        })
+        if (dirErr) {
+            targetDir = `${staticPublicPath}`
+            console.log(` ${date} : moment: createTargetDir: 新建文件夹出错 : ${err}`)
+        }
     }
-  }
-  return targetDir
+    return targetDir
 }
 
 // 格式化新增moment的请求
 async function formatRequest(req) {
-  const form = new formidable.IncomingForm()
-  form.uploadDir = await createTargetDir()
-  form.keepExtensions = true
-  form.multiples = true
-  return new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        reject(err)
-      }
-      const text = fields.text
-      const imgs = Array.isArray(files.imgs)
-        ? files.imgs
-        : files.imgs
-        ? [files.imgs]
-        : []
-      resolve({ text, imgs })
+    const form = new formidable.IncomingForm()
+    form.uploadDir = await createTargetDir()
+    form.keepExtensions = true
+    form.multiples = true
+    return new Promise((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                reject(err)
+            }
+            const text = fields.text
+            const imgs = Array.isArray(files.imgs) ?
+                files.imgs :
+                files.imgs ?
+                [files.imgs] :
+                []
+            resolve({ text, imgs })
+        })
     })
-  })
 }
 
 module.exports = async function(req, res, next) {
-  let content = { imgs: [], text: '' }
-  try {
-    content = await formatRequest(req)
-  } catch (error) {
-    res.send(formatRequest(0, error))
-    return
-  }
+    let content = { imgs: [], text: '' }
+    try {
+        content = await formatRequest(req)
+    } catch (error) {
+        res.send(formatRequest(0, error))
+        return
+    }
 
-  const localImgs = content.imgs.map(img => img.path)
-  let valid = validateMoment(content)
-  if (!valid.isValidate) {
-    res.send(formatResponse(0, valid.message))
-    // 删除已储存的图片
-    localImgs.forEach(img => {
-      fsPromises.unlink(img)
-    })
-    return
-  }
+    const localImgs = content.imgs.map(img => img.path)
+    let valid = validateMoment(content)
+    if (!valid.isValidate) {
+        res.send(formatResponse(0, valid.message))
+        // 删除已储存的图片
+        localImgs.forEach(img => {
+            fsPromises.unlink(img)
+        })
+        return
+    }
 
-  momentModel
-    .create({
-      text: content.text,
-      userId: userId,
-      imgs: localImgs.join(',')
-    })
-    .then(rt => {
-      res.send(formatResponse(1, '发表成功'))
-    })
-    .catch(err => {
-      res.send(formatResponse(0, err))
-    })
+    momentModel
+        .create({
+            text: content.text,
+            userId: userId,
+            imgs: localImgs.join(',')
+        })
+        .then(rt => {
+            res.send(formatResponse(1, '发表成功'))
+        })
+        .catch(err => {
+            res.send(formatResponse(0, err))
+        })
 }
--- server/controller/momentController/controller/listMoment.js
+++ server/controller/momentController/controller/listMoment.js
@@ -1,50 +1,95 @@
 const momentModel = require("../../../Dao").Moment;
 const userModel = require("../../../Dao").User;
 const CommentModel = require("../../../Dao").Comment;
-const fs = require("fs");
-const fsPromises = require("fs").promises
 const { formatResponse } = require("../../../utils");
-const { momentConst, staticPublicPath } = require("../../../conf")["gloableConst"];
-const userId = "0120f580-f92a-11e8-8db7-791c9005fcff";
 
-async function createMomentContent(limit = 10, offset = 0) {
-    // let momentContent = new momentContent()
-    let end = false
-    let momentContent = {}
-    momentModel.findAll({
-        attributes: ['id', 'userId', 'text', 'imgs', 'video', 'updatedAt'],
-        limit: +limit,
-        offset: +offset,
+const momentAttributes = ['id', 'userId', 'text', 'imgs', 'video', 'updatedAt']
+const userAttributes = ['nickName', 'avatar', 'signature', 'id']
+const commentAttributes = ['userId', 'parentCommentId', 'text', 'id', 'updatedAt']
+const commentUserAttributes = ['nickName', 'id']
+
+function createMomentOption(limit, offset) {
+    return {
+        attributes: momentAttributes,
+        limit: limit,
+        offset: offset,
         order: [
             ['updatedAt', 'DESC']
         ],
         include: [
-            { model: userModel, as: 'momentAuthor', attributes: ['nickName', 'avatar', 'signature', 'id'] },
+            { model: userModel, as: 'momentAuthor', attributes: userAttributes },
             {
                 model: CommentModel,
                 as: 'momentComments',
-                attributes: ['userId', 'parentCommentId', 'text', 'id', 'updatedAt'],
+                attributes: commentAttributes,
                 include: [{
                     model: userModel,
                     as: 'commentAuthor',
-                    attributes: ['nickName', 'id']
+                    attributes: commentUserAttributes
                 }]
             },
         ]
-    }).then(result => {
-        console.log('result', result)
-        // let { rows, count } = result
-        // if (offset + limit >= count) {
-        //     end = true
-        // }
-        // rows.forEach(row => {
+    }
+}
 
-        // })
+
+async function findAllMoment(option) {
+    return momentModel.findAll(option).then(result => {
+        let rows = result.map(v => {
+            let row = v.dataValues
+            let { momentAuthor, momentComments } = row
+            momentAuthor = momentAuthor.dataValues
+            momentComments = momentComments.map(mc => mc.dataValues)
+            return row
+        })
+        return rows
+    }).catch(err => {
 
     })
 }
 
+async function countMoment(opiton) {
+    return momentModel.count(opiton).then(res => {
+        return res
+    }).catch(err => {})
+}
+
+
+
+
+async function listMomentContent(limit = 10, offset = 0) {
+    // let momentContent = new momentContent()
+    let end = false
+    limit = +limit
+    offset = +offset
+
+    let findOption = createMomentOption(limit, offset)
+
+    try {
+        // let list = await findAllMoment(findOption)
+        // let total = await countMoment()
+        let [list, total] = await Promise.all([findAllMoment(findOption), countMoment()])
+        if (total < limit + offset) {
+            end = true
+        }
+
+        return {
+            end,
+            list
+        }
+    } catch (error) {
+        console.log('error', error)
+        return false
+    }
+}
+
 module.exports = async function(req, res, next) {
     const { num, start } = req.query
-    createMomentContent(num, start)
+    let rt = await listMomentContent(num, start)
+    if (rt) {
+        res.send(formatResponse(1, rt))
+    } else {
+        res.send(formatResponse(0, '查询错误，请重试'))
+    }
+
 }
