048b1f0280e2f9b02cc879ecc7d74634798518c2
liu.liang <492140398@qq.com>
2019-01-02 15:04:20 +0800

    ll

---
+++ views/src/pages/article/articleDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="article-detail">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+
+  created(){
+
+  },
+
+  methods: {
+
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
